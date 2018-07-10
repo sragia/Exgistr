aba81d8eb9dbced68adbc5b06b1755e4ceb00895
Aigars Bedeicis <aigars.bedeicis@edu.rtu.lv>
2018-07-10 17:54:50 +0300

    a bit of whitespace fix

--- Exgistr/EventHandler.lua
+++ Exgistr/EventHandler.lua
@@ -3,16 +3,16 @@ local LDBI = LibStub("LibDBIcon-1.0")
 local LDB = LibStub:GetLibrary("LibDataBroker-1.1")
 
 local function Init()
-	-- setups start values
-	Exgistr.InitDB()
-	local money = GetMoney()
-	Exgistr.CurrentMoney = money -- Current
-	Exgistr.SessionStart = money -- Session Start
-	Exgistr.SetupCharacter()
-	Exgistr.name = UnitName("player")
-	Exgistr.realm = GetRealmName()
-	Exgistr.InitUI()
-	--Exgistr.CreateTestGUI()
+  -- setups start values
+  Exgistr.InitDB()
+  local money = GetMoney()
+  Exgistr.CurrentMoney = money -- Current
+  Exgistr.SessionStart = money -- Session Start
+  Exgistr.SetupCharacter()
+  Exgistr.name = UnitName("player")
+  Exgistr.realm = GetRealmName()
+  Exgistr.InitUI()
+  --Exgistr.CreateTestGUI()
 end
 
 local f = CreateFrame("frame")
@@ -33,170 +33,170 @@ local lastTransaction
 local transactionType = {}
 local unknownTransactions = {}
 local function ClearTransactionVars()
-	lastTransaction = nil
-	transactionType = nil
+  lastTransaction = nil
+  transactionType = nil
 end
 
 local function GetTransactionType(diff)
-	for key,trans in pairs(transactionType) do
-		if GetTime() - trans.time <= (trans.delay or 1.5) then
-			if trans.expense and diff < 0 then
-				return trans.type
-			elseif trans.income and diff > 0 then
-				return trans.type
-			end
-		end
-	end
-	return diff > 0 and "Looted" or "Unknown"
+  for key,trans in pairs(transactionType) do
+    if GetTime() - trans.time <= (trans.delay or 1.5) then
+      if trans.expense and diff < 0 then
+        return trans.type
+      elseif trans.income and diff > 0 then
+        return trans.type
+      end
+    end
+  end
+  return diff > 0 and "Looted" or "Unknown"
 end
 
 local eventBucket = {}
 local function Bucket(event,func)
-	if not event then return end
-	if eventBucket[event] then
-		if GetTime() - eventBucket[event].time >= 0.5 then
-			eventBucket[event].func()
-			eventBucket[event] = nil
-		end 
-	elseif func then
-		eventBucket[event] = {
-			time = GetTime(),
-			func = func
-		}
-		C_Timer.After(0.51,function() Bucket(event) end)
-	end
+  if not event then return end
+  if eventBucket[event] then
+    if GetTime() - eventBucket[event].time >= 0.5 then
+      eventBucket[event].func()
+      eventBucket[event] = nil
+    end
+  elseif func then
+    eventBucket[event] = {
+      time = GetTime(),
+      func = func
+    }
+    C_Timer.After(0.51,function() Bucket(event) end)
+  end
 end
 
 
 local function PLAYER_MONEY()
-	local current = GetMoney()
-	local diff = current - Exgistr.CurrentMoney
-	if diff ~= 0 then
-		local transType = GetTransactionType(diff)
-		local tId = Exgistr.AddTransaction({
-			amount = diff,
-			type = transType,
-			date = time()--date("*t", time()),
-		})
-		lastTransaction = {id = tId, time = GetTime()}
-	end
-	Exgistr.CurrentMoney = current
+  local current = GetMoney()
+  local diff = current - Exgistr.CurrentMoney
+  if diff ~= 0 then
+    local transType = GetTransactionType(diff)
+    local tId = Exgistr.AddTransaction({
+      amount = diff,
+      type = transType,
+      date = time()--date("*t", time()),
+    })
+    lastTransaction = {id = tId, time = GetTime()}
+  end
+  Exgistr.CurrentMoney = current
 end
 
 
 function f:OnEvent(event,...)
-	if eventFunc[event] then
-		Exgistr.debug.print(event,'happened')
-		eventFunc[event]()
-	end
-	if event == "PLAYER_MONEY" then
-		Bucket(event,PLAYER_MONEY)
-	end
-	if event == "ADDON_LOADED" and ... == name then
-		Exgistr.db = ExgistrDB.db or {}
-		Exgistr.config = ExgistrDB.config or {}
-		Exgistr.config.minimap = Exgistr.config.minimap or {}
-		Exgistr.config.initTime = Exgistr.config.initTime or time()
-		C_Timer.After(0.4,function() Init() end) -- delay a bit
-		LDBI:Register("Exgistr",{
-		  type = "data source",
-		  text = "Exgistr",
-		  icon = "Interface\\AddOns\\Exgistr\\Media\\logo",
-		  OnClick = function() Exgistr.ShowUI() end,
-		},Exgistr.config.minimap)
-		f:UnregisterEvent("ADDON_LOADED")
-	end
-	if event == "PLAYER_LOGOUT" then
-		ExgistrDB.db = Exgistr.GetDB()
-		ExgistrDB.config = Exgistr.config
-	end
+  if eventFunc[event] then
+    Exgistr.debug.print(event,'happened')
+    eventFunc[event]()
+  end
+  if event == "PLAYER_MONEY" then
+    Bucket(event,PLAYER_MONEY)
+  end
+  if event == "ADDON_LOADED" and ... == name then
+    Exgistr.db = ExgistrDB.db or {}
+    Exgistr.config = ExgistrDB.config or {}
+    Exgistr.config.minimap = Exgistr.config.minimap or {}
+    Exgistr.config.initTime = Exgistr.config.initTime or time()
+    C_Timer.After(0.4,function() Init() end) -- delay a bit
+    LDBI:Register("Exgistr",{
+      type = "data source",
+      text = "Exgistr",
+      icon = "Interface\\AddOns\\Exgistr\\Media\\logo",
+      OnClick = function() Exgistr.ShowUI() end,
+    },Exgistr.config.minimap)
+    f:UnregisterEvent("ADDON_LOADED")
+  end
+  if event == "PLAYER_LOGOUT" then
+    ExgistrDB.db = Exgistr.GetDB()
+    ExgistrDB.config = Exgistr.config
+  end
 end
 
 f:SetScript("OnEvent", f.OnEvent)
 
 -- vendoring
 function eventFunc.MERCHANT_UPDATE()
-	local key = "MERCHANT_UPDATE"
-	if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
-		Exgistr.ModifyTransaction(lastTransaction.id,"type","Vendor")
-	else
-		transactionType[key] = {type = "Vendor", time = GetTime(), income = true}
-	end
+  local key = "MERCHANT_UPDATE"
+  if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
+    Exgistr.ModifyTransaction(lastTransaction.id,"type","Vendor")
+  else
+    transactionType[key] = {type = "Vendor", time = GetTime(), income = true}
+  end
 end
 
 -- repair
 function eventFunc.UPDATE_INVENTORY_DURABILITY()
-	local key = "UPDATE_INVENTORY_DURABILITY"
-	if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
-		Exgistr.ModifyTransaction(lastTransaction.id,"type","Repair")
-	else
-		transactionType[key] = {type = "Repair", time = GetTime(), expense = true}
-	end
+  local key = "UPDATE_INVENTORY_DURABILITY"
+  if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
+    Exgistr.ModifyTransaction(lastTransaction.id,"type","Repair")
+  else
+    transactionType[key] = {type = "Repair", time = GetTime(), expense = true}
+  end
 end
 
 function eventFunc.LOOT_SLOT_CLEARED()
-	local key = "LOOT_SLOT_CLEARED"
-	if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
-		Exgistr.ModifyTransaction(lastTransaction.id,"type","Looted")
-	else
-		transactionType[key] = {type = "Looted", time = GetTime(), income = true}
-	end
+  local key = "LOOT_SLOT_CLEARED"
+  if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
+    Exgistr.ModifyTransaction(lastTransaction.id,"type","Looted")
+  else
+    transactionType[key] = {type = "Looted", time = GetTime(), income = true}
+  end
 end
 
 function eventFunc.GARRISON_MISSION_COMPLETE_RESPONSE()
-	local key = "GARRISON_MISSION_COMPLETE_RESPONSE"
-	if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
-		Exgistr.ModifyTransaction(lastTransaction.id,"type","Mission")
-	else
-		transactionType[key] = {type = "Mission", time = GetTime(), income = true, delay = 2.5}
-	end
+  local key = "GARRISON_MISSION_COMPLETE_RESPONSE"
+  if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
+    Exgistr.ModifyTransaction(lastTransaction.id,"type","Mission")
+  else
+    transactionType[key] = {type = "Mission", time = GetTime(), income = true, delay = 2.5}
+  end
 end
 
 function eventFunc.AUCTION_ITEM_LIST_UPDATE()
-	local key = "AUCTION_ITEM_LIST_UPDATE"
-	local type = "Auction"
-	if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
-		Exgistr.ModifyTransaction(lastTransaction.id,"type",type)
-	else
-		transactionType[key] = {type = type, time = GetTime(), expense = true, delay = 2.5}
-	end
+  local key = "AUCTION_ITEM_LIST_UPDATE"
+  local type = "Auction"
+  if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
+    Exgistr.ModifyTransaction(lastTransaction.id,"type",type)
+  else
+    transactionType[key] = {type = type, time = GetTime(), expense = true, delay = 2.5}
+  end
 end
 function eventFunc.QUEST_FINISHED()
-	local key = "QUEST_FINISHED"
-	local type = "Quest"
-	if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
-		Exgistr.ModifyTransaction(lastTransaction.id,"type",type)
-	else
-		transactionType[key] = {type = type, time = GetTime(), income = true}
-	end
+  local key = "QUEST_FINISHED"
+  local type = "Quest"
+  if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
+    Exgistr.ModifyTransaction(lastTransaction.id,"type",type)
+  else
+    transactionType[key] = {type = type, time = GetTime(), income = true}
+  end
 end
 
 function  eventFunc.QUEST_REMOVED()
-	local key = "QUEST_REMOVED"
-	local type = "Quest"
-	if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
-		Exgistr.ModifyTransaction(lastTransaction.id,"type",type)
-	else
-		transactionType[key] = {type = type, time = GetTime(), income = true}
-	end
+  local key = "QUEST_REMOVED"
+  local type = "Quest"
+  if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
+    Exgistr.ModifyTransaction(lastTransaction.id,"type",type)
+  else
+    transactionType[key] = {type = type, time = GetTime(), income = true}
+  end
 end
 
 function  eventFunc.MAIL_SEND_SUCCESS()
-	local key = "MAIL_SEND_SUCCESS"
-	local type = "Mail"
-	if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
-		Exgistr.ModifyTransaction(lastTransaction.id,"type",type)
-	else
-		transactionType[key] = {type = type, time = GetTime(), expense = true}
-	end
+  local key = "MAIL_SEND_SUCCESS"
+  local type = "Mail"
+  if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
+    Exgistr.ModifyTransaction(lastTransaction.id,"type",type)
+  else
+    transactionType[key] = {type = type, time = GetTime(), expense = true}
+  end
 end
 
 function  eventFunc.MAIL_SUCCESS()
-	local key = "MAIL_SUCCESS"
-	local type = "Mail"
-	if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
-		Exgistr.ModifyTransaction(lastTransaction.id,"type",type)
-	else
-		transactionType[key] = {type = type, time = GetTime(), income = true}
-	end
+  local key = "MAIL_SUCCESS"
+  local type = "Mail"
+  if lastTransaction and GetTime() - lastTransaction.time <= 0.1 then
+    Exgistr.ModifyTransaction(lastTransaction.id,"type",type)
+  else
+    transactionType[key] = {type = type, time = GetTime(), income = true}
+  end
 end
--- Exgistr/Exgistr.lua
+++ Exgistr/Exgistr.lua
@@ -1,23 +1,23 @@
 local db = {}
 -- Defaults
 local CHAR_ROW_DEFAULT = {
-	key = "char",
-	values = {
-		name = "",
-		realm = "",
-		class = "",
-		ledger = {},
-		current = 0,
-		startData = {},
-	}
+  key = "char",
+  values = {
+    name = "",
+    realm = "",
+    class = "",
+    ledger = {},
+    current = 0,
+    startData = {},
+  }
 }
 local LEDGER_ROW_DEFAULT = {
-	key = "ledger",
-	values = {
-		amount = 0,
-		type = "",
-		date = 0,	
-	},
+  key = "ledger",
+  values = {
+    amount = 0,
+    type = "",
+    date = 0,
+  },
 }
 
 local function AddMissingTableEntries(data,DEFAULT)
@@ -36,198 +36,198 @@ end
 -- DB Functions
 
 local function ValidateRow(t,default)
-	for key,v in pairs(default.values) do
-		if not t[key] or not type(v) == type(t[key]) then
-			Exgistr.debug.print("Row Validation false. For key",default.key)
-			return false
-		end
-	end
-	Exgistr.debug.print("Row Validation Successful. For key",default.key)
-	return true
+  for key,v in pairs(default.values) do
+    if not t[key] or not type(v) == type(t[key]) then
+      Exgistr.debug.print("Row Validation false. For key",default.key)
+      return false
+    end
+  end
+  Exgistr.debug.print("Row Validation Successful. For key",default.key)
+  return true
 end
 
 local function FindCharacter(name,realm)
-	name = name or UnitName('player')
-	realm = realm or GetRealmName()
-	for id,char in pairs(db) do
-		if char.name == name and char.realm == realm then
-			return id
-		end
-	end
-	return
+  name = name or UnitName('player')
+  realm = realm or GetRealmName()
+  for id,char in pairs(db) do
+    if char.name == name and char.realm == realm then
+      return id
+    end
+  end
+  return
 end
 Exgistr.FindCharacter = FindCharacter
 
 local function AddCharacter(t)
-	local id = #db+1
-	if ValidateRow(t,CHAR_ROW_DEFAULT) then -- validate
-		db[id] = t
-		return id
-	end
+  local id = #db+1
+  if ValidateRow(t,CHAR_ROW_DEFAULT) then -- validate
+    db[id] = t
+    return id
+  end
 end
 
 function Exgistr.SetupCharacter()
-	local realm = GetRealmName()
-	local name = UnitName("player")
-	local _,class = UnitClass("player")
-	local id = FindCharacter(name,realm)
-	if not id then
-		-- first time seeing this character
-		return AddCharacter({
-			name = name,
-			realm = realm,
-			class = class,
-			ledger = {},
-			current = Exgistr.CurrentMoney,
-			startData = {
-				gold = Exgistr.CurrentMoney,
-				date = time()
-			},
-		})
-	elseif db[id].current ~= Exgistr.CurrentMoney then
-		Exgistr.AddTransaction({
-				amount = Exgistr.CurrentMoney-db[id].current,
-				type = "Unknown",
-				date = time(),--date("*t", time()),
-			})
-		db[id].current = Exgistr.CurrentMoney
-	end
+  local realm = GetRealmName()
+  local name = UnitName("player")
+  local _,class = UnitClass("player")
+  local id = FindCharacter(name,realm)
+  if not id then
+    -- first time seeing this character
+    return AddCharacter({
+      name = name,
+      realm = realm,
+      class = class,
+      ledger = {},
+      current = Exgistr.CurrentMoney,
+      startData = {
+        gold = Exgistr.CurrentMoney,
+        date = time()
+      },
+    })
+  elseif db[id].current ~= Exgistr.CurrentMoney then
+    Exgistr.AddTransaction({
+        amount = Exgistr.CurrentMoney-db[id].current,
+        type = "Unknown",
+        date = time(),--date("*t", time()),
+      })
+    db[id].current = Exgistr.CurrentMoney
+  end
 end
 
 local function compare(current,target,operator)
-	if not current or not target then return false end
-	operator = operator or "<"
-	local ret = loadstring(string.format("return %f %s %f", current,operator,target))
-	return ret()
+  if not current or not target then return false end
+  operator = operator or "<"
+  local ret = loadstring(string.format("return %f %s %f", current,operator,target))
+  return ret()
 end
 
 function Exgistr.SelectLedgerData(charId,filter)
-	-- filter = {
-	--	key = "", value = "", compare = ">",
-	-- }
-	local ret = {}
-	if charId == "all" then
-		for id,char in pairs(db) do
-			for i,data in ipairs(char.ledger) do
-				if data[filter.key] then
+  -- filter = {
+  --	key = "", value = "", compare = ">",
+  -- }
+  local ret = {}
+  if charId == "all" then
+    for id,char in pairs(db) do
+      for i,data in ipairs(char.ledger) do
+        if data[filter.key] then
           if compare(data[filter.key],filter.value,filter.compare) then
-						table.insert(ret,data)
-					end
-				end
-			end 
-		end
-	else
-		for i,data in ipairs(db[charId].ledger) do
-			if data[filter.key] then
-				if compare(data[filter.key],filter.value,filter.compare) then
-					table.insert(ret,data)
-				end
-			end
-		end 
-	end
-	return ret
+            table.insert(ret,data)
+          end
+        end
+      end
+    end
+  else
+    for i,data in ipairs(db[charId].ledger) do
+      if data[filter.key] then
+        if compare(data[filter.key],filter.value,filter.compare) then
+          table.insert(ret,data)
+        end
+      end
+    end
+  end
+  return ret
 end
 
-function Exgistr.GetCharacterInfo(charId) 
-	if not db[charId] then return end
-	local char = db[charId]
-	local t = {
-		name = char.name,
-		realm = char.realm,
-		class = char.class,
-		current = char.current,
-		startData = char.startData,
-	}
-	return t
+function Exgistr.GetCharacterInfo(charId)
+  if not db[charId] then return end
+  local char = db[charId]
+  local t = {
+    name = char.name,
+    realm = char.realm,
+    class = char.class,
+    current = char.current,
+    startData = char.startData,
+  }
+  return t
 end
 
 
 -- LEDGER
 function Exgistr.AddTransaction(t,name,realm)
-	local id = FindCharacter(name,realm)
-	if not id then
-		id = Exgistr.SetupCharacter()
-	end
-	if ValidateRow(t,LEDGER_ROW_DEFAULT) then
-		local tId = #db[id].ledger+1
-		db[id].ledger[tId] = t
-		db[id].current = db[id].current + t.amount
-		Exgistr.debug.print("Adding row| Id:",tId," | Source:",t.type)
-		Exgistr.CurrentMoney = db[id].current
-		return tId
-	end
-	Exgistr.debug.print('failed Add')
+  local id = FindCharacter(name,realm)
+  if not id then
+    id = Exgistr.SetupCharacter()
+  end
+  if ValidateRow(t,LEDGER_ROW_DEFAULT) then
+    local tId = #db[id].ledger+1
+    db[id].ledger[tId] = t
+    db[id].current = db[id].current + t.amount
+    Exgistr.debug.print("Adding row| Id:",tId," | Source:",t.type)
+    Exgistr.CurrentMoney = db[id].current
+    return tId
+  end
+  Exgistr.debug.print('failed Add')
 end
 
 function Exgistr.ModifyTransaction(id,key,newValue)
-	local charId = FindCharacter()
-	db[charId].ledger[id][key] = newValue
-	Exgistr.debug.print("Modifing",id,"row:",key,"=",newValue)
+  local charId = FindCharacter()
+  db[charId].ledger[id][key] = newValue
+  Exgistr.debug.print("Modifing",id,"row:",key,"=",newValue)
 end
 
 function Exgistr.GetLedgerData(id)
-	return db[id].ledger
+  return db[id].ledger
 end
 function Exgistr.GetCharacterLedgers(filter)
-	local ret = {}
-	for id,char in pairs(db) do
-		ret[char.realm] = ret[char.realm] or {}
-		for i,l in ipairs(char.ledger) do
-			if filter and l[filter.key] then
-				if compare(l[filter.key],filter.value,filter.compare) then
-					table.insert(ret[char.realm],l)
-				end
-			else
-				table.insert(ret[char.realm],l)
-			end
-		end
-	end
-	return ret
+  local ret = {}
+  for id,char in pairs(db) do
+    ret[char.realm] = ret[char.realm] or {}
+    for i,l in ipairs(char.ledger) do
+      if filter and l[filter.key] then
+        if compare(l[filter.key],filter.value,filter.compare) then
+          table.insert(ret[char.realm],l)
+        end
+      else
+        table.insert(ret[char.realm],l)
+      end
+    end
+  end
+  return ret
 end
 -- GUI
 function Exgistr.GetCharacters(realm)
-	local t = {}
-	for id,char in pairs(db) do
-		if not realm or realm == char.realm then
-			table.insert(t,{name = char.name,class= char.class, gold = char.current, id = id, realm = char.realm})
-		end
-	end
-	return t
+  local t = {}
+  for id,char in pairs(db) do
+    if not realm or realm == char.realm then
+      table.insert(t,{name = char.name,class= char.class, gold = char.current, id = id, realm = char.realm})
+    end
+  end
+  return t
 end
 
 function Exgistr.GetRealms()
-	local t = {}
-	for id,char in pairs(db) do
-		t[char.realm] = t[char.realm] and t[char.realm] + 1 or 1
-	end
-	return t
+  local t = {}
+  for id,char in pairs(db) do
+    t[char.realm] = t[char.realm] and t[char.realm] + 1 or 1
+  end
+  return t
 end
 function Exgistr.GetCharacter(id)
-	if not id or not db[id] then return end
-	return db[id]
+  if not id or not db[id] then return end
+  return db[id]
 end
 
 function Exgistr.GetInitTime()
-	return Exgistr.config.initTime
+  return Exgistr.config.initTime
 end
 -- DB SETUP
 local function CheckDB()
-	-- validates DB
-	local t = db
-	for id,char in pairs(db) do
-		if type(char) ~= 'table' then 
-			t[id] = nil
-		else
-			t[id] = AddMissingTableEntries(char,CHAR_ROW_DEFAULT.values)
-			-- ledger
-			for i,ledge in ipairs(char.ledger) do
-				if not ValidateRow(ledge,LEDGER_ROW_DEFAULT) then
-					t[id].ledger[i] = AddMissingTableEntries(t[id].ledger[i],LEDGER_ROW_DEFAULT.values)
-				end
-			end
-		end
-	end
-	db = t
+  -- validates DB
+  local t = db
+  for id,char in pairs(db) do
+    if type(char) ~= 'table' then
+      t[id] = nil
+    else
+      t[id] = AddMissingTableEntries(char,CHAR_ROW_DEFAULT.values)
+      -- ledger
+      for i,ledge in ipairs(char.ledger) do
+        if not ValidateRow(ledge,LEDGER_ROW_DEFAULT) then
+          t[id].ledger[i] = AddMissingTableEntries(t[id].ledger[i],LEDGER_ROW_DEFAULT.values)
+        end
+      end
+    end
+  end
+  db = t
 end
 
 local function ModernizeDB()
@@ -248,7 +248,7 @@ end
 function Exgistr.InitDB()
   db = Exgistr.db
   ModernizeDB()
-	CheckDB()
+  CheckDB()
 end
 
 function Exgistr.GetDB()
@@ -256,7 +256,7 @@ function Exgistr.GetDB()
 end
 
 function Exgistr.ClearDB()
-	Exgistr.db = {}
-	db = Exgistr.db
-	Exgistr.SetupCharacter()
+  Exgistr.db = {}
+  db = Exgistr.db
+  Exgistr.SetupCharacter()
 end
